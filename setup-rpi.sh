e812482270ae4f0683cf3e36854c1fc7c88fb9dc
Patrick Evans <ppevans11@gmail.com>
2020-07-24 22:47:25 +1200

    :sparkles: Updated setup-rpi.sh to use common scripts

--- setup-rpi.sh
+++ setup-rpi.sh
@@ -150,51 +150,17 @@ go get -u github.com/xxxserxxx/gotop/cmd/gotop
 
 # --------------------------------------------------------------------------------------------------- #
 
-# Node Version Manager (NVM)
-# ==========================
+# NodeJS Installs
+# ===============
 
-curl -o- https://raw.githubusercontent.com/nvm-sh/nvm/v0.35.3/install.sh | bash
-
-# Previous node version
-# previous-node=$(nvm current)
-
-# NodeJS LTS and NPM
-# ------------------
-
-nvm install --lts --latest-npm
-
-# nvm install --lts --latest-npm --reinstall-packages-from=$(nvm current)
-# nvm install --lts --latest-npm --reinstall-packages-from=12.17.0
-# nvm reinstall-packages previous-node
-
-# NPM Global Installs
-# -------------------
-
-npm install --global \
-  add-gitignore \
-  check-it-out \
-  diff-so-fancy \
-  gitmoji-cli \
-  david \
-  npm-check \
-  npm-check-updates \
-  ntl \
-  licensed \
-  neovim \
-  gtop \
-  wifi-password-cli \
-  corona-cli
+bash common/node.sh
 
 # --------------------------------------------------------------------------------------------------- #
 
-# Yarn
-# ====
+# Yarn Installs
+# =============
 
-curl -sS https://dl.yarnpkg.com/debian/pubkey.gpg | sudo apt-key add -
-echo "deb https://dl.yarnpkg.com/debian/ stable main" | sudo tee /etc/apt/sources.list.d/yarn.list
-
-sudo apt update
-sudo apt install -y yarn
+bash common/yarn.sh
 
 # --------------------------------------------------------------------------------------------------- #
 
@@ -205,99 +171,17 @@ sudo apt install -y yarn
 
 # --------------------------------------------------------------------------------------------------- #
 
-# Python Version Manager (pyenv)
-# ==============================
+# Python Installs
+# ===============
 
-# TODO: Update pyenv if already installed
-
-git clone https://github.com/pyenv/pyenv.git ~/.pyenv
-echo 'export PYENV_ROOT="$HOME/.pyenv"' >> ~/.bashrc
-echo 'export PATH="$PYENV_ROOT/bin:$PATH"' >> ~/.bashrc
-echo -e 'if command -v pyenv 1>/dev/null 2>&1; then\n  eval "$(pyenv init -)"\nfi' >> ~/.bashrc
-
-# Restart Shell
-exec "$SHELL"
-
-# Install Dependencies
-# --------------------
-
-sudo apt install --no-install-recommends -y \
-  make \
-  build-essential \
-  libssl-dev \
-  zlib1g-dev \
-  libbz2-dev \
-  libreadline-dev \
-  libsqlite3-dev \
-  llvm \
-  libncurses5-dev \
-  xz-utils \
-  tk-dev \
-  libxml2-dev \
-  libxmlsec1-dev \
-  libffi-dev \
-  liblzma-dev
-
-# Install Python2
-# ---------------
-
-pyenv install 2.7.18
-
-# Install Python3
-# ---------------
-
-pyenv install 3.8.5
-
-# Set Global Python Versions
-# --------------------------
-
-pyenv global 3.8.5 2.7.18 system
-
-pyenv rehash
-
-# Python2 Installs
-# ================
-
-# sudo apt install python2
-# sudo apt install python-pip
-
-python2 -m pip install --upgrade \
-  pip \
-  setuptools \
-  wheel \
-  neovim \
-  virtualenv
-
-# Python3 Installs
-# ================
-
-# sudo apt install python3
-# sudo apt install python3-pip
-
-python3 -m pip install --upgrade \
-  pip \
-  setuptools \
-  wheel \
-  neovim \
-  thefuck
+bash common/python.sh
 
 # --------------------------------------------------------------------------------------------------- #
 
 # Ruby Installs
 # =============
 
-sudo apt install -y ruby-full
-
-# Ruby Gem Installs
-# -----------------
-
-sudo gem update --system
-sudo gem update
-
-sudo gem install \
-  bundler \
-  rails \
-  neovim
+bash common/ruby.sh
 
 # --------------------------------------------------------------------------------------------------- #
 
