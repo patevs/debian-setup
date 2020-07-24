3ed7e9bbd0edc2d2a50718515111528756ac01fd
Patrick Evans <ppevans11@gmail.com>
2020-07-24 22:36:34 +1200

    :sparkles: Updated setup-rpi.sh to use common/rust.sh

--- setup-rpi.sh
+++ setup-rpi.sh
@@ -306,33 +306,6 @@ sudo gem install \
 
 bash common/rust.sh
 
-# Install Rustup
-# --------------
-
-curl --proto '=https' --tlsv1.2 -sSf https://sh.rustup.rs | sh
-
-# Active cargo
-# ------------
-
-source $HOME/.cargo/env
-
-# Update Rustup
-# -------------
-
-rustup self update
-rustup update
-
-# Cargo Installs
-# --------------
-
-cargo install \
-  bat \
-  eva \
-  lsd \
-
-# cargo install git-delta
-# cargo install tokei
-
 # --------------------------------------------------------------------------------------------------- #
 
 # EOF #
