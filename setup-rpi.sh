567315d88dd0561fb206cc2503a635ccefe2cc7a
Patrick Evans <ppevans11@gmail.com>
2020-07-24 20:37:03 +1200

    :bulb: Updated comments in setup-rpi.sh

--- setup-rpi.sh
+++ setup-rpi.sh
@@ -379,11 +379,16 @@ sudo gem install neovim
 #   libffi-dev \
 #   libgdbm-dev \
 #   libncurses5-dev \
+#   libsqlite3-dev \
 #   libtool \
 #   libyaml-dev \
+#   make \
 #   pkg-config \
 #   sqlite3 \
+#   zlib1g-dev \
 #   libgmp-dev \
+#   libreadline-dev \
+#   libssl-dev
 
 # gpg --keyserver hkp://keys.gnupg.net --recv-keys 409B6B1796C275462A1703113804BB82D39DC0E3 7D2BAF1CF37B13E2069D6956105BD0E739499BDB
 # OR:
