1c7f553d2c7866a8cb16a68fb2f875e16e0e67b4
Patrick Evans <ppevans11@gmail.com>
2020-09-29 11:48:55 +1300

    :arrow_up: Bump nvm version 0.35.3 -> 0.36.0

--- common/node.sh
+++ common/node.sh
@@ -11,8 +11,9 @@
 
 # Install Node Version Manager (NVM)
 # ----------------------------------
+# https://github.com/nvm-sh/nvm
 
-curl -o- https://raw.githubusercontent.com/nvm-sh/nvm/v0.35.3/install.sh | bash
+curl -o- https://raw.githubusercontent.com/nvm-sh/nvm/v0.36.0/install.sh | bash
 
 # Previous node version
 # previous-node=$(nvm current)
